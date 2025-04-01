27cfaf7df6f2b0c4ba2318742ec7687fa9b3dc26
Finley Gibson <f.j.gibson@exeter.ac.uk>
2025-04-01 15:33:00 +0100

    added toml config

--- src/phageid/__init__.py
+++ src/phageid/__init__.py
@@ -1,2 +1,9 @@
-from .utils import DIR_ROOT
+from .utils import DIR_ROOT, FILE_CONFIG
 from .logging_config import logging
+import toml
+
+try:
+    config = toml.load(FILE_CONFIG)
+    logging.info(f"Loaded config from {FILE_CONFIG}")
+except Exception as e:
+    logging.error(f"Failed to load config from {FILE_CONFIG}: {e}")
--- src/phageid/utils.py
+++ src/phageid/utils.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
 DIR_ROOT = Path(__file__).parent.parent.parent
+
+FILE_CONFIG = DIR_ROOT / "config.toml"
