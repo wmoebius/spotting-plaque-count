33568e15cde69ffe177db296e863dc901d02c953
Finley Gibson <f.j.gibson@exeter.ac.uk>
2025-04-06 13:22:20 +0100

    standardised layers.py to adhere to input and output types more consistently.

--- src/phageid/processing/layers.py
+++ src/phageid/processing/layers.py
@@ -1,24 +1,30 @@
-from typing import Protocol
-from numpy.typing import NDArray
-import numpy as np
-from typing import Union, Optional, Callable
+from typing import Callable, Optional, Protocol, Tuple, TypeVar, Union
+
 import cv2
-from phageid import logging
-from typing import Tuple
-from .kernels import Kernel
+import numpy as np
+from numpy.typing import NDArray
 from skimage.feature import peak_local_max
+
+from phageid import logging
+from phageid.dtypes import ImageStack, PointStack
 from phageid.utils import image_to_cartesian
 
+from .kernels import Kernel
 
-class Layer(Protocol):
-    """
-    Single detection layer base class.
-    """
+T = TypeVar("T", bound="StackBase")
 
-    def __call__(self, image: NDArray[np.number], **kwargs) -> NDArray[np.number]: ...
+class Layer(Protocol[T]):
+    def __call__(self, stack: T) -> T: ...
 
 
-class Threshold(Layer):
+class ImageLayer(Layer):
+    def __call__(self, stack: ImageStack) -> ImageStack: ...
+
+class PointLayer(Layer):
+    def __call__(self, stack: ImageStack | PointStack) -> ImageStack: ...
+
+
+class Threshold(ImageLayer):
     def __init__(
         self, thresh: np.number, above: bool = True, allow_equal: bool = False
     ):
@@ -26,40 +32,41 @@ class Threshold(Layer):
         self.above = above
         self.allow_equal = allow_equal
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.float64]:
-        op_str = f"{'greater' if self.above else 'less'}{'_equal' if self.allow_equal else ''}"
-        op = getattr(np, op_str)
-        return op(image, self.thresh).astype(np.int64)
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        op = getattr(np, "greater") if self.above else getattr(np, "less")
+        return op(stack, self.thresh).astype(np.int64)
 
 
-class Normalise(Layer):
+class Normalise(ImageLayer):
     def __init__(self, range: Optional[Tuple[np.number, np.number]] = None):
         self.range = range
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.float64]:
-        image = image - image.min(axis=(1, 2))[:, np.newaxis, np.newaxis]
-        image = image / image.max(axis=(1, 2))[:, np.newaxis, np.newaxis]
+    def __call__(self, stack: ImageStack, **kwargs) -> ImageStack:
+        stack_ = np.vstack(stack)
+        stack_ = stack_ - stack_.min(axis=(1, 2))[:, np.newaxis, np.newaxis]
+        stack_ = stack_ / stack_.max(axis=(1, 2))[:, np.newaxis, np.newaxis]
 
         range = self.range if self.range is not None else (0.0, 1.0)
 
         scale = range[1] - range[0]
 
-        image = image * scale + range[0]
+        stack_ = stack_ * scale + range[0]
 
-        return image
+        return stack_
 
 
-class Erosion(Layer):
+class Erosion(ImageLayer):
     def __init__(self, kernel_size: int = 3, iterations: int = 1):
         self.kernel_size = kernel_size
         self.iterations = iterations
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.float64]:
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        stack_ = np.vstack(stack)
         kernel = np.ones((self.kernel_size, self.kernel_size), dtype=np.uint8)
         processed = cv2.erode(
-            image.astype(np.uint8), kernel, iterations=self.iterations
+            stack_.astype(np.uint8), kernel, iterations=self.iterations
         )
-        return processed.astype(np.float64)
+        return [im for im in processed.astype(np.float64)]
 
 
 class Dilation(Layer):
@@ -67,12 +74,13 @@ class Dilation(Layer):
         self.kernel_size = kernel_size
         self.iterations = iterations
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.float64]:
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        stack_ = np.vstack(stack)
         kernel = np.ones((self.kernel_size, self.kernel_size), dtype=np.uint8)
         processed = cv2.dilate(
-            image.astype(np.uint8), kernel, iterations=self.iterations
+            stack_.astype(np.uint8), kernel, iterations=self.iterations
         )
-        return processed.astype(np.float64)
+        return [im for im in processed.astype(np.float64)]
 
 
 class GaussianBlur(Layer):
@@ -80,63 +88,33 @@ class GaussianBlur(Layer):
         self.kernel_size = kernel_size
         self.sigma = sigma
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.float64]:
-        for i, img in enumerate(image):
-            image[i] = cv2.GaussianBlur(
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        for i, img in enumerate(stack):
+            stack[i] = cv2.GaussianBlur(
                 img.astype(np.float32), (self.kernel_size, self.kernel_size), self.sigma
             )
-        return image
+        return stack
 
 
-class Difference(Layer):
+class Difference(ImageLayer):
     def __init__(self, separation: int = 1):
         self.separation = separation
 
-    def __call__(self, images: NDArray[np.number]) -> NDArray[np.float64]:
-        num_images = images.shape[0]
-        if self.separation < 1 or self.separation >= num_images:
-            raise ValueError(f"Separation must be between 1 and {num_images - 1}.")
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        stack_ = np.vstack(stack)
+        num_stacks = stack_.shape[0]
+        if self.separation < 1 or self.separation >= num_stacks:
+            raise ValueError(f"Separation must be between 1 and {num_stacks - 1}.")
 
         differences = [
-            images[i + self.separation] - images[i]
-            for i in range(num_images - self.separation)
+            stack_[i + self.separation] - stack_[i]
+            for i in range(num_stacks - self.separation)
         ]
-        return np.stack(differences, axis=0).astype(np.float64)
+        return differences
 
 
-class ThreshReplace(Layer):
-    def __init__(
-        self, thresh: float, replace: Union[np.number, str], above: bool = True
-    ):
-        self.thresh = thresh
-        self.replace = replace
-        self.above = above
 
-    def __call__(self, images: NDArray[np.number]) -> NDArray[np.float64]:
-        if isinstance(self.replace, str):
-            try:
-                op = getattr(np, self.replace)
-            except AttributeError:
-                raise ValueError(
-                    f"replace value parsed to ThreshReplace not valid. Received {self.replace}"
-                )
-            values = op(images, axis=(1, 2), keepdims=True)
-        else:
-            values = np.ones(images.shape[0]) * self.replace
-
-        if self.above:
-            mask = images > self.thresh
-        else:
-            mask = images < self.thresh
-
-        images[mask] = np.take_along_axis(
-            values, np.zeros_like(images, dtype=int), axis=0
-        )[mask]
-
-        return images
-
-
-class MaskReplace(Layer):
+class MaskReplace(ImageLayer):
     def __init__(
         self,
         mask: NDArray[np.int64],
@@ -146,13 +124,15 @@ class MaskReplace(Layer):
         self.mask = mask
         self.replace = replace
 
-    def __call__(self, images: NDArray[np.number]) -> NDArray[np.float64]:
+    def __call__(self, stack: ImageStack, **kwargs) -> ImageStack:
+        stack_ = np.vstack(stack)
+
         if isinstance(self.replace, Callable):
-            for i in range(images.shape[0]):
-                images[i][self.mask] = self.replace(images[i])
+            for i in range(stack_.shape[0]):
+                stack_[i][self.mask] = self.replace(stack_[i])
         elif isinstance(self.replace, (float, int, np.nan)):
-            for i in range(images.shape[0]):
-                images[i][self.mask] = self.replace
+            for i in range(stack_.shape[0]):
+                stack_[i][self.mask] = self.replace
         else:
             logging.error(
                 "Bad argument to MaskReplace. replace should be number or callable, instead found:",
@@ -162,20 +142,59 @@ class MaskReplace(Layer):
                 f"Bad argument to MaskReplace. replace should be number or callable, instead found: {type(self.replace)}"
             )
 
-        return images
+        return [im for im in stack_]
 
 
-class SubtractByFrame(Layer):
+class ThreshReplace(ImageLayer):
+    def __init__(
+        self, thresh: float, replace: Union[np.number, str], above: bool = True
+    ):
+        self.thresh = thresh
+        self.replace = replace
+        self.above = above
+
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        stack_ = np.stack(stack)
+        if isinstance(self.replace, str):
+            try:
+                op = getattr(np, self.replace)
+            except AttributeError:
+                raise ValueError(
+                    f"replace value parsed to ThreshReplace not valid. Received {self.replace}"
+                )
+            values = op(stack_, axis=(1, 2), keepdims=True)
+        else:
+            values = np.ones(stack_.shape[0]) * self.replace
+
+        if self.above:
+            mask = stack_ > self.thresh
+        else:
+            mask = stack_ < self.thresh
+
+        # TODO: changed the below lines but did not test. Need to test and confirm.
+        # TODO: This could easily be modified to use MaskReplace and reduce codebase
+        # size and increase maintainability.
+        # stack_[mask] = np.take_along_axis(
+        stack_[:, mask] = np.take_along_axis(
+            values, np.zeros_like(stack_, dtype=int), axis=0
+        )[mask]
+
+        return [im for im in stack_]
+
+
+class SubtractByFrame(ImageLayer):
     def __init__(self, value: Callable | np.number):
         self.value = value
 
-    def __call__(self, images: NDArray[np.number]) -> NDArray[np.float64]:
+    def __call__(self, stack: ImageStack, **kwargs) -> ImageStack:
+        stack_ = np.vstack(stack)
+
         if isinstance(self.value, Callable):
-            for i in range(images.shape[0]):
-                images[i] -= self.value(images[i])
+            for i in range(stack_.shape[0]):
+                stack_[i] -= self.value(stack_[i])
         elif isinstance(self.value, (float, int, np.nan)):
-            for i in range(images.shape[0]):
-                images[i] -= self.value
+            for i in range(stack_.shape[0]):
+                stack_[i] -= self.value
         else:
             logging.error(
                 f"Bad argument. value should be number or callable, instead found: {type(self.value)}",
@@ -183,7 +202,7 @@ class SubtractByFrame(Layer):
             raise ValueError(
                 f"Bad argument. value should be number or callable, instead found: {type(self.value)}",
             )
-        return images
+        return [img for img in stack_]
 
 
 class Subtract(Layer):
@@ -193,35 +212,34 @@ class Subtract(Layer):
         self.value = value
         self.operation = operation
 
-    def __call__(self, images: NDArray[np.number]) -> NDArray[np.float64]:
+    def __call__(self, stack: ImageStack, **kwargs) -> ImageStack:
+        stack_ = np.vstack(stack)
         if self.value is not None and self.operation is None:
-            images = images - self.value
+            stacks = stack_ - self.value
         elif self.value is None and self.operation is not None:
-            images = images - self.operation(images)
+            stacks = stack_ - self.operation(stack_)
         else:
             msg = "Subtract layer should provide one of value or operation, not both and not neither."
             logging.error(msg)
             raise ValueError(msg)
-        return images
+        return [img for img in stack_]
 
 
-class Convolution(Layer):
+class Convolution(ImageLayer):
     def __init__(self, kernel: Kernel):
         self.kernel = kernel
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.number]:
-        return np.stack(
-            [self.kernel.convolve(img).astype(np.float64) for img in image], axis=0
-        )
+    def __call__(self, stack: ImageStack) -> ImageStack:
+        return [self.kernel.convolve(img).astype(np.float64) for img in stack]
 
 
-class PeakFinder(Layer):
+class PeakFinder(PointLayer):
     def __init__(self, min_distance: int, threshold_rel: float, threshold_abs: float):
         self.min_distance = min_distance
         self.threshold_rel = threshold_rel
         self.threshold_abs = threshold_abs
 
-    def __call__(self, image: NDArray[np.number]) -> NDArray[np.number]:
+    def __call__(self, stack: ImageStack) -> PointStack:
         peaks = [
             peak_local_max(
                 im,
@@ -229,6 +247,6 @@ class PeakFinder(Layer):
                 threshold_rel=self.threshold_rel,
                 threshold_abs=self.threshold_abs,
             )
-            for im in image
+            for im in stack
         ]
         return [image_to_cartesian(pk) for pk in peaks]
