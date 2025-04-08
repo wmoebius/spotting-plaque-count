c866d9abf1d1387eb450da43f481e8254fa8ac91
Finley Gibson <f.j.gibson@exeter.ac.uk>
2025-04-08 16:37:33 +0100

    updated GaussandDetector in src/phageid/detectors

--- src/phageid/detection/detectors.py
+++ src/phageid/detection/detectors.py
@@ -1,15 +1,16 @@
 from abc import ABC
+import numpy as np
 
 from phageid.dtypes import ImageStack, PointStack
 from phageid.processing import Process
-from phageid.processing.kernels import GaussianKernel, RingKernel
+from phageid.processing.kernels import RingKernel, GaussianKernel
 from phageid.processing.layers import (
     AgglomeratePeaks,
     Convolution,
-    GaussianBlur,
     PeakFinder,
     SubtractByFrame,
     Difference,
+    Threshold,
 )
 from phageid.utils import find_first_peak
 
@@ -22,11 +23,11 @@ class Detector(ABC):
 class GaussianDetector(Detector):
     process = Process(
         [
-            SubtractByFrame(value=lambda x: find_first_peak(x, 256, False)),
-            GaussianBlur(kernel_size=7, sigma=0.5),
-            Convolution(kernel=GaussianKernel(size=13, sigma=2)),
-            GaussianBlur(kernel_size=9, sigma=1),
-            PeakFinder(min_distance=15, threshold_rel=0.0, threshold_abs=32),
+            Threshold(
+                thresh=lambda x: find_first_peak(x) * 1.1, above=True, allow_equal=False
+            ),
+            Convolution(kernel=GaussianKernel(size=21, sigma=3)),
+            PeakFinder(threshold_abs=4.9, footprint=np.ones((3, 3))),
             AgglomeratePeaks(min_distance=15),
         ]
     )
